445a4fb1c124bfce042273ed327abd0d02fb47e9
Sergey Maksimov <m6v@mail.ru>
2025-10-21 00:06:35 +0300

    In XML validator class replace by closure

--- validator.py
+++ validator.py
@@ -1,43 +1,39 @@
 #!/usr/bin/env python3
+import functools
+import jsonschema
 import logging
 from lxml import etree
 
 # Для логирования в файл, добавить filename='app.log' иначе лог в консоль
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
 
-schema_path = "schema.xml"
 msg = """
-<?xml version="1.0" encoding="UTF-8"?>
 <person>
   <id>1</id>
   <name>Sergey Maksimov</name>
-  <age>52</age>
+  <age>22</age>
 </person>
 """
 
-
-class Validator:
-    def __init__(self, schema_path: str):
-        xmlschema_doc = etree.parse(schema_path)
-        self.xmlschema = etree.XMLSchema(xmlschema_doc)
-
-    def validate(self, msg: str) -> bool:
+def validator(schema: str):
+    '''Вернуть функцию валидации xml-схемы, заданной в файле schema'''
+    xmlschema = etree.XMLSchema(etree.parse(schema))
+    def validate(msg: str) -> bool:
         try:
-            xml_doc = etree.fromstring(msg)
-            self.xmlschema.assertValid(xml_doc)
+            xmlschema.assertValid(etree.fromstring(msg))
             logging.debug("Validation is successful")
             return True
         except etree.DocumentInvalid:
-            for error in self.xmlschema.error_log:
-                logging.error("%s in line %d" % (error.message.rstrip("."), error.line))
+            for error in xmlschema.error_log:
+                logging.warning("%s in line %d" % (error.message.rstrip("."), error.line))
         except etree.XMLSyntaxError as err:
-            logging.error(err)
+            logging.warning(err)
         return False
-
+    return validate
 
 def main():
-    validator = Validator(schema_path)
-    validator.validate(msg)
+    validate = validator("schema.xml")
+    validate(msg)
 
 
 if __name__ == "__main__":
